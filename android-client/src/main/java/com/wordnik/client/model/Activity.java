e157af5388d630331a9ab07fa02e5824abb6e29b
Sean Mikalson <sean.mikalson@gmail.com>
2015-02-03 21:06:16 -0700

    Update sdk

--- android-client/src/main/java/com/wordnik/client/model/Activity.java
+++ android-client/src/main/java/com/wordnik/client/model/Activity.java
@@ -8,6 +8,8 @@ import com.wordnik.client.model.Time;
 public class Activity {
   @JsonProperty("activity_id")
   private String activity_id = null;
+  @JsonProperty("Name")
+  private String Name = null;
   @JsonProperty("description")
   private String description = null;
   @JsonProperty("max_attendees")
@@ -27,6 +29,13 @@ public class Activity {
   }
   public void setActivity_id(String activity_id) {
     this.activity_id = activity_id;
+  }
+
+  public String getName() {
+    return Name;
+  }
+  public void setName(String Name) {
+    this.Name = Name;
   }
 
   public String getDescription() {
@@ -83,6 +92,7 @@ public class Activity {
     StringBuilder sb = new StringBuilder();
     sb.append("class Activity {\n");
     sb.append("  activity_id: ").append(activity_id).append("\n");
+    sb.append("  Name: ").append(Name).append("\n");
     sb.append("  description: ").append(description).append("\n");
     sb.append("  max_attendees: ").append(max_attendees).append("\n");
     sb.append("  creator: ").append(creator).append("\n");
